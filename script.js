06b66cc2be277f5363377b2964149a04e032d9c0
richard <richardnaoufal@example.com>
2021-06-14 15:00:05 +0100

    manage local storage

--- script.js
+++ script.js
@@ -61,3 +61,26 @@ function updateSelectedCount() {
 // Initial count and total set
 updateSelectedCount();
 
+// ####################
+// local storage
+// ####################
+
+function populateUI() {
+  const selectedSeats = JSON.parse(localStorage.getItem('selectedSeats'));
+
+  if (selectedSeats !== null && selectedSeats.length > 0) {
+    seats.forEach((seat, index) => {
+      if (selectedSeats.indexOf(index) > -1) {
+        seat.classList.add('selected');
+      }
+    });
+  }
+
+  const selectedMovieIndex = localStorage.getItem('selectedMovieIndex');
+
+  if (selectedMovieIndex !== null) {
+    movieSelect.selectedIndex = selectedMovieIndex;
+  }
+}
+
+populateUI();
