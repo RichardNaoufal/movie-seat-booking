71e70f165eab23a311b7ca12bf4e80c037c7a347
richard <richardnaoufal@example.com>
2021-06-14 14:28:27 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+// ####################
+// select Movie
+// ####################
+
+const movieSelect = document.getElementById('movie');
+
+let ticketPrice = movieSelect.value;
+
+movieSelect.addEventListener('change', e => {
+  ticketPrice = +e.target.value;
+  setMovieData(e.target.selectedIndex, e.target.value);
+  updateSelectedCount();
+});
+
+// Save selected movie index and price
+function setMovieData(movieIndex, moviePrice) {
+  localStorage.setItem('selectedMovieIndex', movieIndex);
+  localStorage.setItem('selectedMoviePrice', moviePrice);
+}
+
